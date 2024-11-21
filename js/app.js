3aab8ce56f348cfb8d71e99c82ae89437b20ef95
aqqullo <xaqqulloabdusamotov@gmail.com>
2024-11-21 21:12:01 +0500

    ff

--- js/app.js
+++ js/app.js
@@ -21,7 +21,19 @@ function closeMenuOnClickOutside(event) {
     window.removeEventListener('click', closeMenuOnClickOutside);
   }
 }
+const menu = document.querySelector(".menu");
+const menuMobile = document.querySelector(".menu-mobile");
+const close = document.querySelector(".close");
 
+menu.addEventListener("click", () => {
+  menuMobile.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+  menuMobile.classList.remove("active");
+  document.body.style.overflowY = "auto";
+});
 
 
 
